6c44311bc54ccdd87bc0789b84b4c51962a40820
The Vinh Ly <lythevinh002@gmail.com>
2022-07-23 15:29:59 +0930

    update unit test

--- UnitTest/Ulogin_test.py
+++ UnitTest/Ulogin_test.py
@@ -2,6 +2,7 @@
 # Unit test for login
 
 import sys
+
 sys.path.insert(0,'..')
 
 from loginModel import loginModel
@@ -10,4 +11,4 @@ new_login = loginModel(filename="../users.json")
 new_login.loaddata()
 
 def test_user_exist():
-    assert new_login.checkUserAvailable(email="asbc@gmail.com") == True
+    assert new_login.checkUserAvailable(email="abc@gmail.com") == True

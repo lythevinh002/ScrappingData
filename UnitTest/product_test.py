2f88f80cd72bca837942495329efd1c02ff6fcb2
The Vinh Ly <lythevinh002@gmail.com>
2022-07-23 16:04:27 +0930

    pull request to merge to main

---
+++ UnitTest/product_test.py
@@ -0,0 +1,15 @@
+# Author: The Vinh Ly
+# Unit test for login
+
+import sys
+
+sys.path.insert(0,'..')
+
+from loginModel import loginModel
+
+new_login = loginModel(filename="../users.json")
+new_login.loaddata()
+
+def test_user_exist():
+    print('hello from feature-y')
+    assert new_login.checkUserAvailable(email="abc@gmail.com") == True

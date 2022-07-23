4092d66a7d0de2f4ee3c756734b1324f49bb9f1f
The Vinh Ly <lythevinh002@gmail.com>
2022-07-23 14:57:20 +0930

    upload unit test

---
+++ loginModel.py
@@ -0,0 +1,24 @@
+import json
+
+class loginModel:
+    def __init__(self, filename, data = []) -> None:
+        self.filename = filename
+        self.data = data
+
+    # Load the data from database
+    def loaddata(self):
+        f = open(self.filename)
+        self.data = json.load(f)
+        f.close()
+
+    # test the existing user
+    def checkUserAvailable(self, email):
+        for i in self.data["users"]:
+            print(i)
+            if i["email"] == email:
+                return True
+        return False
+
+# users = loaddata('users.json')
+
+# print(checkUserAvailable(users["users"], "abc@gmail.com"))
